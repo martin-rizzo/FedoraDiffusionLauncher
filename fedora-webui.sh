6055a97272436c1202d3d0afa101bc6598ca4938
Martin Rizzo <martinrizzo@gmail.com>
2023-03-11 12:41:56 -0300

    refactor: improve funct and vars names + documentation

--- fedora-webui.sh
+++ fedora-webui.sh
@@ -1,28 +1,42 @@
 #!/usr/bin/env bash
+#  Bash script to download and run stable diffusion resolving compatibility
+#  issues between Fedora and AUTOMATIC1111's Stable Diffusion Web UI.
+#  https://github.com/martin-rizzo/FedoraDiffusionLauncher
+#  by Martin Rizzo
 
-OLD_PYTHON='python3.10'
-VIRT_ENV_DIR='.venv-'$OLD_PYTHON
+# Compatible version of python (must be <= 3.10)
+COMP_PYTHON='python3.10'
 WEBUI_SH_URL='https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh'
+VIRT_ENV_DIR='.venv-'$COMP_PYTHON
 
-## ANSI CODES ##
-RED='\033[31m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-NC='\033[0m'
 
+# Function that allows printing messages with different formats.
+# Usage: echoex [check|error|wait] <message>
+# Arguments:
+#   - check: shows the message in green with a checkmark symbol in front.
+#   - error: shows the message in red with an X symbol in front.
+#   - wait : shows the message in yellow with a dash symbol in front.
+#   - message: the message to be displayed.
+#
 function echoex() {
     if [[ $1 == check ]]; then
-        echo -e "$GREEN \xE2\x9C\x94 $2$NC"
+        echo -e "\033[32m \xE2\x9C\x94 $2\033[0m"
     elif [[ $1 == error ]]; then
-        echo -e "$RED x $2$NC"
+        echo -e "\033[31m x $2\033[0m"
     elif [[ $1 == wait ]]; then
-        echo -e "$YELLOW - $2...$NC"
+        echo -e "\033[33m - $2...\033[0m"
     else
         echo -e "$1"
     fi
 }
 
-function command_exists() {
+# Function that checks whether a given command is available in the system
+# and prints an error message with installation instructions if it is not.
+# Usage: ensure_command <command>
+# Arguments:
+#   - command: the name of the command to be checked.
+#
+function ensure_command() {
     if ! command -v $1 &> /dev/null; then
         echoex error "$1 is not available!"
         echoex "   you can try to install '$1' using the following command:"
@@ -33,36 +47,55 @@ function command_exists() {
     fi
 }
 
+# Function that downloads a file from a remote URL and ensures that
+# it is available locally.
+# Usage: ensure_download <local_file> <remote_url>
+# Arguments:
+#   - local_file: the name and path of the file to be saved locally.
+#   - remote_url: the URL of the file to be downloaded.
+#
+function ensure_download() {
+    local local_file=$1 remote_url=$2
+    if [[ ! -e "$local_file" ]]; then
+        echoex wait 'downloading'
+        wget -q --show-progress "$remote_url"
+        if [[ ! -f "$local_file" ]]; then
+            echo error "can not download $local_file\n"
+            exit 1
+        fi
+        echoex check "$local_file downloaded"
+    else
+        echoex check "$local_file already downloaded"
+    fi
+    chmod a+x "$local_file"
+}
+
+# Function that checks whether a virtual environment exists, and creates
+# a new one if it doesn't.
+# Usage: ensure_virt_env <venv_dir> <python>
+# Arguments:
+#   - venv_dir: the path of the virtual environment dir to be checked.
+#   - python  : the Python interpreter that will create the v. environment.
+#
 function ensure_virt_env() {
-    if [[ ! -d "$VIRT_ENV_DIR" ]]; then
+    local venv_dir=$1 python=$2
+    if [[ ! -d "$venv_dir" ]]; then
         echoex wait 'creating virtual environment'
-        "$OLD_PYTHON" -m venv "$VIRT_ENV_DIR"
+        "$python" -m venv "$venv_dir"
         echoex check 'new virtual environment created:'
-        echoex  "     $PWD/$VIRT_ENV_DIR"
+        echoex  "     $venv_dir"
     else
         echoex check 'virtual environment already exists'
     fi
 }
 
-function ensure_download() {
-    local local_name=$1 remote_url=$2
-    echoex wait 'downloading'
-    wget -q --show-progress "$remote_url"
-    if [[ ! -f "$local_name" ]]; then
-        echo error "can not download $local_name]\n"
-        exit 1
-    fi
-    chmod a+x "$local_name"
-    echoex check "$local_name downloaded"
-}
-
-command_exists wget
-command_exists git
-command_exists $OLD_PYTHON
+ensure_command wget
+ensure_command git
+ensure_command "$COMP_PYTHON"
 ensure_download webui.sh "$WEBUI_SH_URL"
-ensure_virt_env
+ensure_virt_env "$PWD/$VIRT_ENV_DIR" "$COMP_PYTHON"
 
-echoex check "activating virtual environment with $OLD_PYTHON"
+echoex check "activating virtual environment with $COMP_PYTHON"
 source "$VIRT_ENV_DIR/bin/activate"
 
 echoex wait 'launching webui.sh'
