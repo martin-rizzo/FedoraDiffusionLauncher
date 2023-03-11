7bd75e90a10a56f1cbae07d1952f4d1dbcbb1608
Martin Rizzo <martinrizzo@gmail.com>
2023-03-10 21:43:42 -0300

    feat: download and run the 'webui.sh' script from AUTOMATIC1111 repository

--- fedora-webui.sh
+++ fedora-webui.sh
@@ -2,10 +2,12 @@
 
 OLD_PYTHON='python3.10'
 VIRT_ENV_DIR='.venv-'$OLD_PYTHON
+WEBUI_SH_URL='https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh'
 
 ## ANSI CODES ##
 RED='\033[31m'
 GREEN='\033[32m'
+YELLOW='\033[33m'
 NC='\033[0m'
 
 function echoex() {
@@ -14,7 +16,7 @@ function echoex() {
     elif [[ $1 == error ]]; then
         echo -e "$RED x $2$NC"
     elif [[ $1 == wait ]]; then
-        echo -n "   $2..."
+        echo -e "$YELLOW - $2...$NC"
     else
         echo -e "$1"
     fi
@@ -42,14 +44,27 @@ function ensure_virt_env() {
     fi
 }
 
+function ensure_download() {
+    local local_name=$1 remote_url=$2
+    echoex wait 'downloading'
+    wget -q --show-progress "$remote_url"
+    if [[ ! -f "$local_name" ]]; then
+        echo error "can not download $local_name]\n"
+        exit 1
+    fi
+    chmod a+x "$local_name"
+    echoex check "$local_name downloaded"
+}
+
 command_exists wget
 command_exists git
 command_exists $OLD_PYTHON
+ensure_download webui.sh "$WEBUI_SH_URL"
 ensure_virt_env
 
 echoex check "activating virtual environment with $OLD_PYTHON"
 source "$VIRT_ENV_DIR/bin/activate"
 
-# echoex wait 'launching webui.sh'
-# python_cmd=$(which python) ./webui.sh
+echoex wait 'launching webui.sh'
+python_cmd=$(which python) ./webui.sh
 
